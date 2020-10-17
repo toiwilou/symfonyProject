ec2e2b44503f3de0695ce8a308674f2ec2fc0e65
root <root@localhost.localdomain>
2020-10-18 00:44:26 +0200

    creation of home's block on reservation

--- src/Controller/CoursesController.php
+++ src/Controller/CoursesController.php
@@ -154,12 +154,44 @@ class CoursesController extends AbstractController
             }
         }
 
+        // Tableau d'élements des reservations
+        $reservations = [
+            "res1" => [
+                "header" => "Prendre un cour régulier",
+                "description" => "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                proident, sunt in culpa qui officia deserunt mollit anim id est laborum." 
+            ],
+            "res2" => [
+                "header" => "Prendre un cours ponctuel",
+                "description" => "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                proident, sunt in culpa qui officia deserunt mollit anim id est laborum." 
+            ],
+            "res3" => [
+                "header" => "Reserver un stage",
+                "description" => "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                proident, sunt in culpa qui officia deserunt mollit anim id est laborum." 
+            ]
+        ];
+
         return $this->render('courses/index.html.twig', [
             "courses" => $courses,
             "date" => $date,
             "nextCourse" => $nextCourse,
             "description" => $desc,
             "title" => $title,
+            "reservations" => $reservations,
         ]);
     }
 }
