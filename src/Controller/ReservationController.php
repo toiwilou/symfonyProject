c8898f575e27e9f313205e898e8f8d531c9cb4c9
root <root@localhost.localdomain>
2020-10-19 22:56:22 +0200

    success of initiation of send mail without content

--- src/Controller/ReservationController.php
+++ src/Controller/ReservationController.php
@@ -4,16 +4,28 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 class ReservationController extends AbstractController
 {
     /**
      * @Route("/reservation", name="reservation")
      */
-    public function index()
+    public function index(\Swift_Mailer $mailer)
     {
+        $req = Request::createFromGlobals();
+        $test = var_dump($req->request);
+
+        $tester = $req->request->get("firstname");
+
+        $message = (new \Swift_Message('Nouveau contact'))->setFrom("toiwilouhassane@gmail.com")->setTo('toiwismart@gmail.com')
+        ->setBody("salut je suis un test de " . $req->request->get("firstname"));
+
+        $mailer->send($message);
+
         return $this->render('reservation/index.html.twig', [
-            'controller_name' => 'ReservationController',
+            "test" => $test,
+            "tester" => $tester
         ]);
     }
 }
